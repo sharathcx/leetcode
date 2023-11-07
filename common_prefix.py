d8d95630110fc062b525f384099ac17933fc54af
Iamsharath007 <sharathpournami@gmail.com>
2023-11-07 19:51:52 +0530

    common prefix partial answer

--- common_prefix.py
+++ common_prefix.py
@@ -1,25 +1,34 @@
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
+        min_index = 0
         common_str = ""
-        minimum = 1
-        for i in range(len(strs) - 1):
-            minimum = len(strs[i])
-            if minimum > len(strs[i + 1]):
-                minimum = len(strs[i + 1])
+        if len(strs) == 0:
+            return ""
+        elif len(strs) == 1:
+            return strs[0]
+        else:
+            minimum = len(strs[0])
+            for i in range(len(strs) - 1):
+                l = len(strs[i])
+                s = strs[i]
+                if minimum > len(strs[i + 1]):
+                    minimum = len(strs[i+1])
+                    min_index = i + 1
+            if minimum == 0:
+                return ""
 
-        for i in range(len(strs)):
-            flag = True
-            for j in range(len(strs)):
-                s1 = strs[i][:minimum]
-                s2 = strs[j][:minimum]
-                if strs[i][:minimum] != strs[j][:minimum]:
-                    flag = False
-            if flag:
-                return strs[i][:minimum]
-            else:
-                minimum -= 1
-        return ""
+            for i, letter in enumerate(strs[min_index]):
+                flag = True
+                for j in range(len(strs)):
+                    if strs[j][i] != letter:
+                        flag = False
+                if flag:
+                    # if letter not in common_str:
+                    common_str += letter
+                else:
+                    return common_str
+            return common_str
 
 
 s = Solution()
-print(s.longestCommonPrefix(["cir", "car"]))
+print(s.longestCommonPrefix(["flower","flow","flight"]))
